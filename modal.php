f920a385499ab5717160241e7cc342d4d84b5785
Joetin4 <49560101+Joetin4@users.noreply.github.com>
2019-05-02 08:56:10 +0200

    Initial Commit

---
+++ modal.php
@@ -0,0 +1,31 @@
+    <!-- MODAL CONTENT STARTS HERE -->
+
+    <div class="modal fade text-center" id="imgModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog modal-lg" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="imgModalLabel">Detaljer</h5>
+
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+
+          </div>
+          <div class="modal-body">
+            <div class="row" id="imgModalContent">
+
+                <div class="col-sm-6" id="imgDetails"> </div>
+
+                <div class="col-sm-6" id="imgInfo"> </div>
+
+            </div>
+          </div>
+          <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Stäng</button>
+        <!--<button type="button" class="btn btn-primary">Save changes</button> -->
+      </div>
+    </div>
+  </div>
+</div>
+
+    <!-- END MODAL CONTENT -->
---
+++ showImages.php
@@ -0,0 +1,120 @@
+<script>
+
+  function showImages(str) {
+
+    
+    /* Om strängen är tom.. */
+    if (str.length == 0) {
+      document.getElementById("imgSuggestion").innerHTML = "";
+      return;
+    }
+
+    var xhttp = new XMLHttpRequest();
+
+    xhttp.onreadystatechange = function() {
+
+      if (this.readyState == 4 && this.status == 200) {
+
+        var x = this.responseText;
+        //document.getElementById("imgSuggestion").innerHTML = x;
+
+        //split up the results and store in array
+        var array = x.split(",");
+
+        //going thtough the array
+        var htmlText = "";
+
+        //om arrayen är tom så returneras "No results found.."
+        if(x === "no suggestion"){
+
+          htmlText = "No results found.."
+
+        }else{
+
+              for (var i = 0; i < array.length; i++) {
+
+                  htmlText += '<div class="float-left" id="test'
+                  htmlText += array[i];
+                  htmlText += '" onmouseover="aFunctionOver(this.id)" onmouseout="aFunctionOut(this.id)"> <a id='
+                  htmlText += array[i];
+                  htmlText += ' href= "" onclick="showImageDetails(this.id)" data-toggle="modal" data-target="#imgModal"> <img class="rounded img-fluid border" src="./img/';
+                  htmlText += array[i];
+                  htmlText += '.jpg" hspace="20" width="330" length="330"> </a> </div>';
+
+                }
+            }
+
+          document.getElementById("imgSuggestion").innerHTML = htmlText;
+
+      }
+    };
+
+    xhttp.open("GET", "answer.php?q="+str, true);
+
+    xhttp.send();
+  }
+
+  /* Denna funktion visar detaljerad information i modalen */
+  function showImageDetails(str) {
+    //alert("input is: " + str);
+    var xhttp;
+  //  if (str.length == 0) {
+  //    document.getElementById("txtHint").innerHTML = "";
+  //    return;
+  //  }
+    xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      //Make sure its ready
+      if (this.readyState == 4 && this.status == 200) {
+
+        var x = this.responseText;
+
+        //alert("response is: "+ x);
+        //document.getElementById("txtHint").innerHTML = x;
+        //split up the results and store in array
+        var array = x.split(",");
+        //going thtough the array
+        var htmlImg = "";
+        var htmlText = "";
+        if(x === "no suggestion"){
+
+          htmlText = "No results found.."
+
+        }else{
+          //The image
+          htmlImg = '<img class="rounded img-fluid border" src="./img/'
+          htmlImg += x;
+          htmlImg += '.jpg" hspace="20" width="300" length="300">'
+          //The information about the picture
+          //htmlText = 'Photographer: ';
+          //htmlText += '<br>Date taken: ';
+          //htmlText += '<br>Times left to be used: ';
+          htmlText = '<div class=container> <table class="table"> <thead> <tr> <th scope="col" bgcolor="#bdbdbd"> </th> <th scope="col" bgcolor="#bdbdbd"> </th> </tr> </thead> <tbody> <tr> <th scope="row">Title: </th> <td>'
+          htmlText += x;
+          htmlText += '</td> </tr> <tr> <th scope="row">Photographer: </th><td> ? </td> </tr><tr><th scope="row">Times left to be used: </th><td> ? </td></tr></tbody></table> </div>';
+        }
+
+          /* Manipulerar element i modalen */
+          document.getElementById("imgDetails").innerHTML = htmlImg;
+          document.getElementById("imgInfo").innerHTML = htmlText;
+
+
+      }
+    };
+    xhttp.open("GET", "answer.php?q="+str, true);
+    xhttp.send();
+  }
+
+  //ON HOVER, input=id
+  function aFunctionOver(x){
+
+  document.getElementById(x).style.backgroundColor ="#bdbdbd";
+
+  }
+  //Change color back to white
+  function aFunctionOut(x){
+
+  document.getElementById(x).style.backgroundColor ="#FFFFFF";
+  }
+
+</script>
